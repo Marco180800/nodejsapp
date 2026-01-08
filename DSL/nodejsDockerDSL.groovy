8d6d9a9a4397c20a98c567e5e5957f390c780edf
Marco180800 <marcovargas1808@outlook.com>
2026-01-08 11:48:03 -0600

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/Marco180800/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('Marco180800')
+            node / gitConfigEmail('marcovargas1808outlook.com')
         }
     }
     triggers {
